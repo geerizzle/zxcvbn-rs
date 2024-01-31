6eca838ff03953a7238e0530bf5ddc7643154094
Dejan Dudukovic <dekidudukovic@gmail.com>
2024-01-31 21:51:29 +0100

    Fixed dumb error

--- src/consts/keys.rs
+++ src/consts/keys.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 lazy_static! {
     static ref KEYS_MAP: HashMap<&'static str, Vec<Option<&'static str>>> = {
-        let mut map: HashMap<&str, Option<Vec<&str>>> = HashMap::new();
+        let mut map: HashMap<&str, Vec<Option<&str>>> = HashMap::new();
         map.insert("q", vec![Some("w"), Some("a"), None, Some("1")]);
         map
     };
